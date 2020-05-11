f9802ad43111b0c5e83c768850d7d2e088d6870c
carrot030 <yaatoo0413@gmail.com>
2020-05-11 10:31:34 +0800

    選項視窗嘗試更新

---
+++ Assets/Scripts/button.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour
+{
+    private GameManager_s gamanager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gamanager=GameObject.Find("GameManager").GetComponent<GameManager_s>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void onclickone()
+    {
+        gamanager.optionstart();
+    }
+    public void onclicktwo()
+    {
+        gamanager.optionend();
+    }
+}
