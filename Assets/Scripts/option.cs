cc61d0320bff829e6380f2f0737704ab070b9d45
carrot030 <yaatoo0413@gmail.com>
2020-09-26 16:42:50 +0800

    嘗試使用gitignore的版本

---
+++ Assets/Scripts/option.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class option : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void enter ()
+    {
+        transform.localPosition = new Vector3(0, 0,0);
+    }
+    public void exit()
+    {
+        transform.localPosition = new Vector3(0,500,0);
+    }
+}
